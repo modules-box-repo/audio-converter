44af6fb576509f8a1e209946b77d27683464c076
YasserNull <yasserbnull@gmail.com>
2026-09-21 07:32:02 +0100

    .

--- README.md
+++ README.md
@@ -5,8 +5,8 @@
 A tool for converting audio formats, supporting multiple formats via FFmpeg.
 
 <p align="center">
-  <img src="https://img.shields.io/github/downloads/null-box-repo/audio-converter/total?label=Downloads"/>
-  <img src="https://img.shields.io/github/v/release/null-box-repo/audio-converter?include_prereleases&label=Release"/>
+  <img src="https://img.shields.io/github/downloads/modules-box-repo/audio-converter/total?label=Downloads"/>
+  <img src="https://img.shields.io/github/v/release/modules-box-repo/audio-converter?include_prereleases&label=Release"/>
   <img src="https://img.shields.io/badge/License-GPLv3-blue.svg"/>
 </p>
 
@@ -21,6 +21,6 @@ TAK, DSF, G722, G723.1, G726, G728, G729, iLBC, aptX, aptX HD, SBC, LC3, Codec2
 ---
 
 <p align="center">
-  <img src="https://github.com/null-box-repo/audio-converter/blob/main/docs/images/screenshot1.jpg" width="45%">
-  <img src="https://github.com/null-box-repo/audio-converter/blob/main/docs/images/screenshot2.jpg" width="45%">
+  <img src="https://github.com/modules-box-repo/audio-converter/blob/main/docs/images/screenshot1.jpg" width="45%">
+  <img src="https://github.com/modules-box-repo/audio-converter/blob/main/docs/images/screenshot2.jpg" width="45%">
 </p>
--- setup.sh
+++ setup.sh
@@ -1,42 +1,35 @@
 #!/bin/sh
 
+set -e
+
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
 NC='\033[0m'
 
-echo -e "${BLUE}[1/5] Updating packages...${NC}"
-apk update && echo -e "${GREEN}[1/5] Update complete${NC}"
+echo -e "${BLUE}[1/4] Updating packages...${NC}"
+apk update && echo -e "${GREEN}[1/4] Update complete${NC}"
 
-echo -e "${BLUE}[2/5] Upgrading packages...${NC}"
-apk upgrade && echo -e "${GREEN}[2/5] Upgrade complete${NC}"
+echo -e "${BLUE}[2/4] Upgrading packages...${NC}"
+apk upgrade && echo -e "${GREEN}[2/4] Upgrade complete${NC}"
 
-echo -e "${BLUE}[3/5] Checking nodejs...${NC}"
+echo -e "${BLUE}[3/4] Checking nodejs...${NC}"
 if command -v node &> /dev/null; then
-    echo -e "${GREEN}[3/5] nodejs is already installed${NC}"
+    echo -e "${GREEN}[3/4] nodejs is already installed${NC}"
 else
-    echo -e "${YELLOW}[3/5] nodejs not found${NC}"
-    echo -e "${BLUE}[3/5] Installing nodejs...${NC}"
-    apk add nodejs && echo -e "${GREEN}[3/5] nodejs installed successfully${NC}"
+    echo -e "${YELLOW}[3/4] nodejs not found${NC}"
+    echo -e "${BLUE}[3/4] Installing nodejs...${NC}"
+    apk add nodejs && echo -e "${GREEN}[3/4] nodejs installed successfully${NC}"
 fi
 
-echo -e "${BLUE}[4/5] Checking npm...${NC}"
-if command -v npm &> /dev/null; then
-    echo -e "${GREEN}[4/5] npm is already installed${NC}"
-else
-    echo -e "${YELLOW}[4/5] npm not found${NC}"
-    echo -e "${BLUE}[4/5] Installing npm...${NC}"
-    apk add npm && echo -e "${GREEN}[4/5] npm installed successfully${NC}"
-fi
-
-echo -e "${BLUE}[5/5] Checking FFmpeg...${NC}"
+echo -e "${BLUE}[4/4] Checking ffmpeg...${NC}"
 if command -v ffmpeg &> /dev/null; then
-    echo -e "${GREEN}[5/5] FFmpeg is already installed${NC}"
+    echo -e "${GREEN}[4/4] ffmpeg is already installed${NC}"
 else
-    echo -e "${YELLOW}[5/5] FFmpeg not found${NC}"
-    echo -e "${BLUE}[5/5] Installing FFmpeg...${NC}"
-    apk add ffmpeg && echo -e "${GREEN}[5/5] FFmpeg installed successfully${NC}"
+    echo -e "${YELLOW}[4/4] ffmpeg not found${NC}"
+    echo -e "${BLUE}[4/4] Installing ffmpeg...${NC}"
+    apk add ffmpeg && echo -e "${GREEN}[4/4] ffmpeg installed successfully${NC}"
 fi
 
 echo -e "${GREEN}All done.${NC}"
